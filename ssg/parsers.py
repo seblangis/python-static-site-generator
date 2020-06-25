5772872e5df9c6e2b23e4bd9e05f53d8200a8eaa
Sébastien Langis <sebastien.langis@gmail.com>
2020-06-24 23:45:28 -0400

    2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -23,7 +23,7 @@ class Parser:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(path, dest / source.relative_to(path))
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):
